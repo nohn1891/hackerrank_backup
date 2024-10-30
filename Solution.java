403cd2c99d024e050be2ee5b6d9e1a880cc41200
nohn1891 <nohn1891@gmail.com>
2024-10-30 14:18:38 +0700

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution{
+	
+	 public int diagonalDifference(List<List<Integer>> arr) {
+		 System.out.println(arr);
+        int a = 0 ; 
+        int b = 0 ;
+        for(int i = 0 ; i<arr.size(); i++){
+			a += arr.get(i).get(i);
+			b += arr.get(i).get((arr.size()-i)-1);
+		}
+        System.out.println(a);
+		System.out.println(b);
+		c = a-b;
+		if(c < 0 ){
+			return c*-1;
+		}
+        return c;
+    }
+	
+	public static void main(String[] args) throws IOException {
+	
+		        Random random = new Random();
+		 int n = 3;
+		
+         List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                int arrItem =  random.nextInt(10);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+		Solution solution = new Solution();
+		System.out.println(solution.diagonalDifference(arr));
+		
+    }
+	
+	
+}
